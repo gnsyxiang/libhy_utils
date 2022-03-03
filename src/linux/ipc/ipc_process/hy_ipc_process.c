5ea42b11a30b12f990e0c202a1ef7c723b393192
zhenquan.qiu <gnsyxiang@163.com>
2022-03-03 14:12:46 +0800

    refactor: refactor ipc_process dir

---
+++ src/linux/ipc/ipc_process/hy_ipc_process.c
@@ -0,0 +1,36 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_ipc_process.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    03/03 2022 10:22
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        03/03 2022      create the file
+ * 
+ *     last modified: 03/03 2022 10:22
+ */
+#include <stdio.h>
+
+#include "hy_hal/hy_assert.h"
+#include "hy_hal/hy_log.h"
+
+#include "hy_ipc_process.h"
+
+void HyIpcProcessDestroy(void **ipc_process_h)
+{
+
+}
+
+void *HyIpcProcessCreate(HyIpcProcessConfig_s *config)
+{
+
+    return NULL;
+}
