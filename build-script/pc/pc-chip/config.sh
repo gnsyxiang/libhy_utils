a3ca30d8ddb721186a5021751c3d24c309b89e15
zhenquan.qiu <gnsyxiang@163.com>
2022-03-25 09:08:24 +0800

    refactor: refactor build-script

---
+++ build-script/pc/pc-chip/config.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+chip=pc-chip
+
+product=HY-pc
+
+language=cn en
+
+gcc_version=x86_64-linux-gnu
+host=
+cross_gcc_path=
+
+configure_param=
+
+cppflag=-fstack-protector-all
+cflag=
+cxxflag=
+ldflag=-rdynamic
+lib=
+
+install_path=/opt/data/nfs/install/pc/pc-chip
+
