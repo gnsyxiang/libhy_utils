d0074dc63b6c242524129363e2be5c815810950c
zhenquan.qiu <gnsyxiang@163.com>
2022-03-29 10:50:11 +0800

    build: export include_com and lib_com

--- build-script/eeasytech/SV823/config.sh
+++ build-script/eeasytech/SV823/config.sh
@@ -9,7 +9,7 @@ language=cn en
 host=arm-linux-gnueabihf
 cross_gcc_path=/opt/toolchains/eeasytech/SV823/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
-configure_param=
+configure_param=--enable-cjson
 
 cppflag=-fstack-protector-all
 cflag=
@@ -17,5 +17,5 @@ cxxflag=
 ldflag=-rdynamic
 lib=
 
-install_path=/opt/data/nfs/install/eeasytech/SV823
+install_path=/mnt/data/xia/app-fw/platform/eeasytech/SV823/3rd_lib
 
