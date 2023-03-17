055ceb36410160ad14d6ac96bef77c8f40d73c7c
gnsyxiang <gnsyxiang@163.com>
2023-03-17 14:41:08 +0800

    build: add the choice of vender and chip

---
+++ build-script/pc/pc-chip/config.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+chip=pc-chip
+
+product=HY-pc
+
+language=cn en
+
+host=
+cross_gcc_path=
+
+configure_param=
+
+cppflag=-fstack-protector-all
+cflag=
+cxxflag=
+ldflag=-rdynamic
+lib=
+
+install_path=/mnt/nfs/pc/pc-chip
+
