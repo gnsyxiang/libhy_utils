ed0d6b980e189bbd2a6d1346a614f362939012ba
gnsyxiang <gnsyxiang@163.com>
2023-03-18 10:04:22 +0800

    feat: add hy_template_demo.c

---
+++ example/hy_template_demo.c
@@ -0,0 +1,123 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_template_demo.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    25/10 2021 19:56
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        25/10 2021      create the file
+ * 
+ *     last modified: 25/10 2021 19:56
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "hy_type.h"
+#include "hy_mem.h"
+#include "hy_string.h"
+#include "hy_signal.h"
+#include "hy_module.h"
+#include "hy_utils.h"
+#include "hy_log.h"
+
+#define _APP_NAME "hy_template_demo"
+
+typedef struct {
+    hy_s32_t    is_exit;
+} _main_template_s;
+
+static void _signal_error_cb(void *args)
+{
+    LOGE("------error cb\n");
+
+    _main_template_s *context = args;
+    context->is_exit = 1;
+}
+
+static void _signal_user_cb(void *args)
+{
+    LOGW("------user cb\n");
+
+    _main_template_s *context = args;
+    context->is_exit = 1;
+}
+
+static void _bool_module_destroy(void)
+{
+    HyModuleDestroyBool_s bool_module[] = {
+        {"signal",          HySignalDestroy },
+        {"log",             HyLogDeInit     },
+    };
+
+    HY_MODULE_RUN_DESTROY_BOOL(bool_module);
+}
+
+static hy_s32_t _bool_module_create(_main_template_s *context)
+{
+    HyLogConfig_s log_c;
+    HY_MEMSET(&log_c, sizeof(log_c));
+    log_c.fifo_len                  = 10 * 1024;
+    log_c.save_c.level              = HY_LOG_LEVEL_TRACE;
+    log_c.save_c.output_format      = HY_LOG_OUTFORMAT_ALL;
+
+    int8_t signal_error_num[HY_SIGNAL_NUM_MAX_32] = {
+        SIGQUIT, SIGILL, SIGTRAP, SIGABRT, SIGFPE,
+        SIGSEGV, SIGBUS, SIGSYS, SIGXCPU, SIGXFSZ,
+    };
+
+    int8_t signal_user_num[HY_SIGNAL_NUM_MAX_32] = {
+        SIGINT, SIGTERM, SIGUSR1, SIGUSR2,
+    };
+
+    HySignalConfig_t signal_c;
+    HY_MEMSET(&signal_c, sizeof(signal_c));
+    HY_MEMCPY(signal_c.error_num, signal_error_num, sizeof(signal_error_num));
+    HY_MEMCPY(signal_c.user_num, signal_user_num, sizeof(signal_user_num));
+    signal_c.save_c.app_name        = _APP_NAME;
+    signal_c.save_c.coredump_path   = "./";
+    signal_c.save_c.error_cb        = _signal_error_cb;
+    signal_c.save_c.user_cb         = _signal_user_cb;
+    signal_c.save_c.args            = context;
+
+    HyModuleCreateBool_s bool_module[] = {
+        {"log",         &log_c,         (HyModuleCreateBoolCb_t)HyLogInit,          HyLogDeInit},
+        {"signal",      &signal_c,      (HyModuleCreateBoolCb_t)HySignalCreate,     HySignalDestroy},
+    };
+
+    HY_MODULE_RUN_CREATE_BOOL(bool_module);
+}
+
+int main(int argc, char *argv[])
+{
+    _main_template_s *context = NULL;
+    do {
+        context = HY_MEM_MALLOC_BREAK(_main_template_s *, sizeof(*context));
+
+        if (0 != _bool_module_create(context)) {
+            printf("_bool_module_create failed \n");
+            break;
+        }
+
+        LOGE("version: %s, data: %s, time: %s \n", "0.1.0", __DATE__, __TIME__);
+
+        while (!context->is_exit) {
+            sleep(1);
+        }
+    } while (0);
+
+    _bool_module_destroy();
+    HY_MEM_FREE_PP(&context);
+
+    return 0;
+}
+
