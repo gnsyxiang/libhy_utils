172b33dd5f9f1cd381aa0967759e86cdf5516a44
zhenquan.qiu <gnsyxiang@163.com>
2023-08-17 10:58:48 +0800

    refactor: add run_os for linux and mcu

--- build.sh
+++ build.sh
@@ -155,6 +155,7 @@ get_com_config()
     cppflag="${cppflag} -Wno-error=unused-parameter -Wno-unused-parameter"
     cppflag="${cppflag} -Wno-error=unused-result -Wno-unused-result"
     cppflag="${cppflag} -Wno-error=unused-variable"
+    cppflag="${cppflag} -Wno-error=unused-function"
 
     ldflag="${ldflag} -Wl,--gc-sections"
     ldflag="${ldflag} -Wl,--as-needed"
