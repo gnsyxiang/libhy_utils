5521d143184785586fa6ee7a07f45b6cd2d46d1f
zhenquan.qiu <gnsyxiang@163.com>
2024-02-19 16:59:58 +0800

    feat: add hy_utils.h for autotools check

---
+++ include/hy_utils.h
@@ -0,0 +1,37 @@
+/**
+ *
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_utils.h
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    19/02 2024 16:57
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        19/02 2024      create the file
+ * 
+ *     last modified: 19/02 2024 16:57
+ */
+#ifndef __LIBHY_UTILS_INCLUDE_HY_UTILS_H_
+#define __LIBHY_UTILS_INCLUDE_HY_UTILS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief 用于autotools检查libhy_utils使用
+ */
+void HyUtilsCheck(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/hy_utils.c
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Release under GPLv-3.0.
+ * 
+ * @file    hy_utils.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    19/02 2024 16:58
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        19/02 2024      create the file
+ * 
+ *     last modified: 19/02 2024 16:58
+ */
+#include <stdio.h>
+
+#include "hy_utils.h"
+
+void HyUtilsCheck(void) {}
